ece0f249f8c60e06fadba087bf445a8f040e5bcf
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-03-28 00:39:49 -0300

    Add Container component for layout and update App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import Button from "./components/button";
+import Container from "./components/container";
 import Text from "./components/text";
 
 const App = () => {
-  return <div className="flex gap-2 justify-center items-center p-5 flex-col">
-    <Text variant="body-lg-bold" className="text-gray-100!">HairDay</Text>
-    <Button className="w-full">Teste</Button>
-    <Button className="w-full" disabled>Teste 2</Button>
-  </div>;
+  return (
+    <Container as="main" className="">
+      <Text variant="body-lg-bold" className="text-gray-100!">
+        HairDay
+      </Text>
+      <Button className="w-full">Teste</Button>
+      <Button className="w-full" disabled>
+        Teste 2
+      </Button>
+    </Container>
+  );
 };
 
 export default App;
