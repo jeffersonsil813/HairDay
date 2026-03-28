1dbd1c07a39ef5043e4a00d4f801a701bff4b661
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-03-28 03:34:10 -0300

    Refactor containerVariants to remove redundant mx-auto class from lg size variant for cleaner styling

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,10 +1,10 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import React from "react";
 
-export const containerVariants = cva("", {
+export const containerVariants = cva("mx-auto", {
   variants: {
     size: {
-      lg: "max-w-360 p-3 mx-auto",
+      lg: "max-w-360 p-3",
       md: "max-w-226.5 px-28 py-20",
       sm: "max-w-124.5 p-20",
     },
