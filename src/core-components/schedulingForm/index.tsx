11f0acc6ba12db8b5b0077266b7d25f90bef7054
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-03-31 20:18:22 -0300

    Refactor SchedulingForm to include InputDate and TimeSlotRadioGroup components, and implement form submission handling

--- src/core-components/schedulingForm/index.tsx
+++ src/core-components/schedulingForm/index.tsx
@@ -1,13 +1,19 @@
 import { useTranslation } from "react-i18next";
 import UserIcon from "../../assets/icons/UserSquare.svg?react";
+import Button from "../../components/button";
 import Container from "../../components/container";
+import InputDate from "../../components/input-date";
 import InputText from "../../components/input-text";
 import Text from "../../components/text";
 import Header from "./header";
-import InputDate from "../../components/input-date";
+import TimeSlotRadioGroup from "./timeSlotRadioGroup";
 
 const SchedulingForm = () => {
   const { t } = useTranslation();
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
 
   return (
     <Container
@@ -17,7 +23,7 @@ const SchedulingForm = () => {
     >
       <Header />
 
-      <form>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6">
         <div className="flex flex-col space-y-1">
           <Text variant="body-lg-bold" className="text-gray-100!">
             {t("form.title")}
@@ -27,9 +33,29 @@ const SchedulingForm = () => {
           </Text>
         </div>
 
-        {/* <InputText svg={UserIcon} name="client" /> */}
+        <div className="flex flex-col gap-2">
+          <Text variant="body-md-bold" className="text-gray-200!">
+            {t("form.labels.date")}
+          </Text>
+          <InputDate required />
+        </div>
+
+        <TimeSlotRadioGroup />
+
+        <div className="flex flex-col gap-2">
+          <Text variant="body-md-bold" className="text-gray-200!">
+            {t("form.labels.client")}
+          </Text>
+          <InputText
+            svg={UserIcon}
+            name="client"
+            placeholder={t("form.placeholders.client")}
+            required
+          />
+        </div>
+
+        <Button type="submit">{t("form.submit_button")}</Button>
       </form>
-        {/* <InputDate /> */}
     </Container>
   );
 };
