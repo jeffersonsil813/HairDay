20610c20b5c0640c44a77b2b268f9c3d980d6be2
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-04-01 10:41:29 -0300

    Implement Schedules component with date selection and localization

--- src/core-components/schedules/index.tsx
+++ src/core-components/schedules/index.tsx
@@ -1,7 +1,31 @@
+import { useState, type ChangeEvent } from "react";
+import { useTranslation } from "react-i18next";
 import Container from "../../components/container";
+import InputDate from "../../components/input-date";
+import Text from "../../components/text";
+import { getCurrentDate } from "../../utils/getCurrentDate";
 
 const Schedules = () => {
-  return <Container size="md" className="w-full gap-8">Schedules</Container>;
+  const { t } = useTranslation();
+  const [date, setDate] = useState<string>(getCurrentDate());
+
+  const handleChangeDate = (e: ChangeEvent<HTMLInputElement>) => {
+    setDate(e.target.value);
+  };
+
+  return (
+    <Container size="md" className="w-full gap-8">
+      <div className="flex gap-3">
+        <div className="flex flex-col gap-1 flex-1">
+          <Text variant="body-lg-bold" className="text-gray-100!">
+            {t("schedule.title")}
+          </Text>
+          <Text className="text-gray-300!">{t("schedule.subtitle")}</Text>
+        </div>
+        <InputDate defaultValue={date} onChange={handleChangeDate} />
+      </div>
+    </Container>
+  );
 };
 
 export default Schedules;
